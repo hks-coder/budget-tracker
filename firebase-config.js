ec62f1ec264714dcc64ac4646ac18d52f48d707a
hks-coder <h.sharma10@hotmail.fr>
2026-02-19 10:09:42 +0100

    Update Firebase configuration settings

--- firebase-config.js
+++ firebase-config.js
@@ -12,12 +12,12 @@
 // See FIREBASE_SETUP.md for detailed instructions
 
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "budget-tracker-XXXX.firebaseapp.com",
-  projectId: "budget-tracker-XXXX",
-  storageBucket: "budget-tracker-XXXX.appspot.com",
-  messagingSenderId: "XXXXXXXXXXXX",
-  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  apiKey: "AIzaSyDIvmse7XcSzpiA7nHezqBIl7x6EcGTpqs",
+  authDomain: "budget-tracker-ec900.firebaseapp.com",
+  projectId: "budget-tracker-ec900",
+  storageBucket: "budget-tracker-ec900.firebasestorage.app",
+  messagingSenderId: "1059770697591",
+  appId: "1:1059770697591:web:de2f6ffd8bea5af4af4372"
 };
 
 // Note: The Firebase SDK will be loaded from CDN in index.html
